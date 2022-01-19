030b32dd289837e8e442207afb70427be1313e74
Jawa53 <JawaRakoto@gmail.com>
2022-01-19 11:24:31 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var settings = document.getElementById(settings);
+var transparent = document.getElementsByClassName(transparent);
+var map = document.getElementById(map);
+var profil = document.getElementsByClassName(profil);
+
+
+
